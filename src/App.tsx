2920e7bae5f04592f54fd6bc85635a9e380ca1f9
Nahuel Perdomo <nahuperdomo123@gmail.com>
2025-09-06 12:13:27 -0300

    Inicializacion de proyecto React + Vite + Tailwind | Estructura modular inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center">
+        <h1 className="text-5xl font-extrabold text-white drop-shadow-lg">
+          Estructura proyecto inicial 🚀
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
